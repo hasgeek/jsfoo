9b152907adae2af4fa86d4f3d88d89f3dd4c8d44
Rajasekharreddy sagamreddy <sekhar662@gmail.com>
2015-07-29 17:38:00 +0530

    Init

---
+++ js/map.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([12.8918869, 77.5841651], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([12.8918869, 77.5841651]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
