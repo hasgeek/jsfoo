c8ab9e7c9f45f8e054b077e09a8b98877adf8403
Vidya Ramakrishnan <vidya@hasgeek.in>
2015-08-06 16:44:26 +0530

    Added a site navbar.

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,24 @@
-	// var $cog = $('#cog'),
-	// $body = $(document.body);
+$(document).ready(function() {
+    var siteNavTop = $(".site-navbar").offset().top;
 
-	// $(window).scroll(function() {
-	// 	$cog.css({
-	// 		'transform': 'rotate(' + $body.scrollTop() + 'deg)'
-	// 	});
-	// });
+    $(window).scroll(function() {
+        if($(this).scrollTop() > siteNavTop) {
+            $(".site-navbar").addClass("nav-fixed");
+        }
+        else {
+            $(".site-navbar").removeClass("nav-fixed");
+        }
+    });
+
+    $('.smooth-scroll').click(function(event) {
+        event.preventDefault();
+        var section = $(this).attr('href');
+        if((window).width > 768) {
+            var sectionPos = $(""+section).offset().top - $('.site-navbar').height();
+        }
+        else {
+            var sectionPos = $(""+section).offset().top;
+        }
+        $('html,body').animate({scrollTop:sectionPos}, '900');
+    });
+});
